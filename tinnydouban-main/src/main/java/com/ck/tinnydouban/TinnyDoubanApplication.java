ff8d16a13112b6fc0c9d23ab94c3a30432a0748a
ck76 <chengkun0804@gmail.com>
2022-08-02 18:30:24 +0900

    update

--- tinnydouban-main/src/main/java/com/ck/tinnydouban/TinnyDoubanApplication.java
+++ tinnydouban-main/src/main/java/com/ck/tinnydouban/TinnyDoubanApplication.java
@@ -2,6 +2,8 @@ package com.ck.tinnydouban;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class TinnyDoubanApplication {
--- tinnydouban-mybatis/src/main/java/com/ck/tinnydouban/MybatisApplication.java
+++ tinnydouban-mybatis/src/main/java/com/ck/tinnydouban/MybatisApplication.java
@@ -1,5 +1,15 @@
 package com.ck.tinnydouban;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MybatisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+        System.out.println("hhhhhhhh");
+    }
+
 }
